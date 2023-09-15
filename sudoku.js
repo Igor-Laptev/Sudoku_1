6313c1f84a724040b7561793394f341f474240a0
Igor_L <igor101188@gmail.com>
2023-09-15 12:05:42 +0300

    fixed

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,7 @@
 const { log } = require("console");
 const fs = require("fs");
 const { EOL } = require("os");
+
 function read() {
   const text = fs.readFileSync("./puzzles.txt", "utf-8").trim().split(`${EOL}`);
   return text.map((el) => el.split(""));
@@ -18,6 +19,7 @@ function isSolved() {
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
    * Возвращает булевое значение — решено это игровое поле или нет.
    */
+  //
 }
 
 function prettyBoard() {
